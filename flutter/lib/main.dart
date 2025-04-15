917f4d93d7aa6b9cae996645068177278b16a24b
soddies <ktrn.pchelkina@gmail.com>
2025-04-15 09:18:34 +0300

    Create main.dart

---
+++ flutter/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Первый проект'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _isPressed = false;
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _isPressed = !_isPressed; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) { // выполнено Екатериной Пчелкиной
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Ты нажал на кнопку такое количество раз:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _decrementCounter,
+        tooltip: 'Вычитание',
+        backgroundColor: _isPressed ? Colors.red : Colors.blue, 
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
